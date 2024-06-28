9879332b3f7df5dbd61ef4e5411328d840792039
Tahmidul <tahmidulislam998@gmail.com>
2024-06-28 23:15:23 +0600

    circular_link_list_delete_last_node

---
+++ Link_List/circular_link_ list/Delete_at_last_node.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+struct Node {
+
+    int data;
+
+    struct Node* next;
+};
+Node* Head = NULL;
+
+void Traverse() {
+
+    struct Node* ptr = Head;
+
+    do {
+
+        cout << ptr->data << " -> ";
+
+        ptr = ptr->next;
+
+    } while (ptr != Head);
+
+    cout<<ptr->data<<endl;
+
+}
+void Insert_at_end(int data) {
+
+    struct Node* Newnode = (struct Node*)malloc(sizeof(struct Node));
+
+    if (Newnode == NULL) {
+
+        cout << "Memory Allocation Failed" << endl;
+
+        return;
+    }
+    Newnode->data = data;
+
+    Newnode->next = NULL;
+
+    if (Head == NULL) {
+
+        Head = Newnode;
+
+        Newnode->next = Head;
+
+        return;
+    }
+
+    struct Node* temp = Head;
+
+    while (temp->next != Head) {
+
+        temp = temp->next;
+    }
+    temp->next = Newnode;
+
+    Newnode->next = Head;
+}
+void Delete_at_end() {
+
+    if (Head == NULL) {
+
+        cout << "List is empty" << endl;
+
+        return;
+    }
+    if (Head->next == Head) {
+
+        free(Head);
+
+        Head = NULL;
+
+        return;
+    }
+    struct Node* temp = Head;
+
+    struct Node* prev = NULL;
+
+    while (temp->next != Head) {
+
+        prev = temp;
+
+        temp = temp->next;
+    }
+    prev->next = Head;
+
+    free(temp);
+}
+int main() {
+
+    int n; cin >> n;
+
+    int random;
+
+    while (n--) {
+
+        cin >> random;
+
+        Insert_at_end(random);
+    }
+    Traverse();
+
+    Delete_at_end();
+
+    cout << "after deleting last node: \n" << endl;
+
+    Traverse();
+
+    return 0;
+}
+
