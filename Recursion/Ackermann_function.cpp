8f4fc9acd9001786698b8dc2b27ab91135334ea0
Tahmidul <tahmidulislam998@gmail.com>
2024-06-28 20:37:37 +0600

    Ackermann_function

---
+++ Recursion/Ackermann_function.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+#define int long long
+#define improve ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define endl '\n'
+
+int Ackerman(int m, int n)
+{
+	if (m == 0){
+
+		return n + 1;
+	}
+	else if(m > 0 && n == 0){
+
+		return Ackerman(m - 1, 1);
+	}
+	else if(m > 0 && n > 0){
+
+		return Ackerman(m - 1, Ackerman(m, n - 1));
+	}
+}
+
+signed main()
+{
+	int A;
+
+	A = Ackerman(1, 3);
+
+	cout << A << endl;
+
+	return 0;
+}
+
+
