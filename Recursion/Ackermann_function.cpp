0eeb86abe555e3719a8a565dd5116bab1d51b87d
Tahmidul <tahmidulislam998@gmail.com>
2024-06-28 23:27:53 +0600

    Ackermann_function

--- Recursion/Ackermann_function.cpp
+++ Recursion/Ackermann_function.cpp
@@ -10,18 +10,17 @@ int Ackerman(int m, int n)
 {
 	if (m == 0){
 
-		return n + 1;
+    return n + 1;
 	}
 	else if(m > 0 && n == 0){
 
-		return Ackerman(m - 1, 1);
+    return Ackerman(m - 1, 1);
 	}
 	else if(m > 0 && n > 0){
 
-		return Ackerman(m - 1, Ackerman(m, n - 1));
+    return Ackerman(m - 1, Ackerman(m, n - 1));
 	}
 }
-
 signed main()
 {
 	int A;
